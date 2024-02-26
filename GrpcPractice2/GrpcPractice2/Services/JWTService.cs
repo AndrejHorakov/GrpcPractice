739035aa1dcb2c804f7d0a8a19d06c3085356f37
AndrejHoryakov <andrejhorakov47232@gmail.com>
2024-02-26 21:00:26 +0300

    it need for start server

--- GrpcPractice2/GrpcPractice2/Services/JWTService.cs
+++ GrpcPractice2/GrpcPractice2/Services/JWTService.cs
@@ -6,6 +6,6 @@ public class JWTService : JwtGenerator.JwtGeneratorBase
 {
     public override Task GetJWToken(TokenRequest request, IServerStreamWriter<TokenData> responseStream, ServerCallContext context)
     {
-        
+        return base.GetJWToken(request, responseStream, context);
     }
 }
